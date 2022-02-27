7668f5dff0620db9aac2df2bd41ef6252f919e54
RajMaity <66725088+RajMaity@users.noreply.github.com>
2022-02-28 00:29:27 +0530

    Add files via upload

---
+++ demo_program.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Demo Program</title>
+</head>
+<body>
+	<?php
+	echo "<h1>Prints heading 1 in php</h1><br>";
+	echo "<p>This is a paragraph in php</p><br>";
+	echo "Simple echo statement in php";
+	echo "<br>";
+	?>
+</body>
+</html>
