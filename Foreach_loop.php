d30b51fbb2c5d24fa21df3b5c2fbcd0eabe7adee
RajMaity <66725088+RajMaity@users.noreply.github.com>
2022-03-08 20:34:32 +0530

    Add files via upload

---
+++ Foreach_loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>For each loop</title>
+</head>
+<body>
+	<?php
+		$colors=array("red","green","blue","yellow");
+		foreach($colors as $value){
+			echo "$value<br>";
+		} 
+	?>
+
+</body>
+</html>
